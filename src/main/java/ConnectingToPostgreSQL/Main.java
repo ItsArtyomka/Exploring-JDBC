9af428c41004523f146ca2f01115d5ffa81260ce
Artyom Pionov <pionovartem11@gmail.com>
2023-08-06 23:44:50 +0100

    First Commit: Learning how Java code is used to work with DB(PostgreSQL) via JDBC.

--- src/main/java/ConnectingToPostgreSQL/Main.java
+++ src/main/java/ConnectingToPostgreSQL/Main.java
@@ -2,9 +2,9 @@ package ConnectingToPostgreSQL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     // DB Connection URL
@@ -21,21 +21,26 @@ public class Main {
 
             // SQL Queries
             String exampleSqlQuery = "SELECT * FROM test;";
-            Statement statement = connection.createStatement(); // Creating object needed to send SQL queries.
+            // Normal Statement Initialisation
+//            Statement statement = connection.createStatement(); // Creating object needed to send SQL queries.
+
             // One way to execute statement.
 //            boolean isExecuted = statement.execute(exampleSqlQuery); // Boolean SQL Execution Logic.
 //            if (isExecuted) {
 //                System.out.println("SELECT Statement Executed");
 //            }
-            // Another way to execute statement.
-            ResultSet resultSet = statement.executeQuery(exampleSqlQuery);
-            System.out.println("ID");
-            System.out.println("||---||");
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt("ID"));
+
+            // PrepareStatement Initialisation
+            try(PreparedStatement preparedStatement = connection.prepareStatement(exampleSqlQuery)) { // IMPORTANT to close Statement to save progress. Try-with-resources makes it easier to do.
+                // Another way to execute statement.
+                ResultSet resultSet = preparedStatement.executeQuery(exampleSqlQuery);
+                System.out.println("ID");
+                System.out.println("||---||");
+                while (resultSet.next()){
+                    System.out.println(resultSet.getInt("ID"));
+                }
+                System.out.println("||---||");
             }
-            System.out.println("||---||");
-            statement.close(); // IMPORTANT to close Statement to save progress.
         } catch (SQLException sqlException) {
             System.out.printf("%s\n", conerr); // Text IF connection is NOT Successful.
             System.out.println("SQL Exception: " + sqlException.getMessage());
