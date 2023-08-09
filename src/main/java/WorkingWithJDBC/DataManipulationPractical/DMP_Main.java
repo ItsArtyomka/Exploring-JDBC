8b66a4382050fd6a3096f000ae88f3e4466922bf
Artyom Pionov <pionovartem11@gmail.com>
2023-08-09 23:33:57 +0100

    New Commit: Learning how Java code is used to work with DB(PostgreSQL) via JDBC.

--- src/main/java/WorkingWithJDBC/DataManipulationPractical/DMP_Main.java
+++ src/main/java/WorkingWithJDBC/DataManipulationPractical/DMP_Main.java
@@ -2,24 +2,28 @@ package WorkingWithJDBC.DataManipulationPractical;
 
 import WorkingWithJDBC.PSGSQL_User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 public class DMP_Main {
     private final static String HOST = PSGSQL_User.getHOST();
     private final static String DATABASENAME = PSGSQL_User.getDATABASENAME();
     private final static String USERNAME = PSGSQL_User.getUSERNAME();
     private final static String PASSWORD = PSGSQL_User.getPASSWORD();
+    static Connection connection;
 
-    @SuppressWarnings("ConstantValue")
     public static void main(String[] args) {
         // URL for DB Connection
-        String url = "jdbc:postgresql://" + HOST + "/" + DATABASENAME + "?user=" + USERNAME + "&password=" + PASSWORD;
+        String URL = "jdbc:postgresql://" + HOST + "/" + DATABASENAME + "?user=" + USERNAME + "&password=" + PASSWORD;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+            sqlException.printStackTrace();
+        }
+
         // Connection to the DB
         try {
-            Connection connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
             connection.setAutoCommit(false);
             if (connection == null)
                 System.err.println("No connection with DB.");
@@ -27,10 +31,34 @@ public class DMP_Main {
                 System.out.println("Connected to DB.");
             }
 
+            if (checkValue(new Scanner(System.in).nextInt())) {
+                System.out.println("Number exists already.");
+            } else {
+                System.out.println("Number doesn't exists.");
+            }
+
+            if (insertValue(new Scanner(System.in).nextInt())) {
+                System.out.println("Number added to DB.");
+            } else {
+                System.out.println("Number not added to DB.");
+            }
+
             // SQL Query Processing
-            String SQL = "UPDATE test set name='Alex' where ID=10;"; // Example SQL Query
+            String SQL = "INSERT INTO test(ID,name) VALUES(?,?);"; // Example SQL Query
             try (PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
-                preparedStatement.executeUpdate();
+                preparedStatement.setInt(1, 10);
+                preparedStatement.setString(2, "Olaf");
+                preparedStatement.addBatch();
+
+                preparedStatement.setInt(1, 11);
+                preparedStatement.setString(2, "Erik");
+                preparedStatement.addBatch();
+
+                preparedStatement.setInt(1, 12);
+                preparedStatement.setString(2, "Alex");
+                preparedStatement.addBatch();
+
+                int[] count = preparedStatement.executeBatch();
                 connection.commit();
                 System.out.println("Transaction is Successful.");
             } catch (SQLException sqlException) {
@@ -40,6 +68,39 @@ public class DMP_Main {
             }
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("LoopStatementThatDoesntLoop")
+    public static boolean checkValue(int checkedValue) {
+        String SQL = "SELECT * FROM test WHERE ID=?;";
+        try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setInt(1, checkedValue);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+    @SuppressWarnings("RedundantIfStatement")
+    public static boolean insertValue(int insertedValue) {
+        String SQL = "INSERT INTO test(id) VALUES(?)";
+        try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setInt(1, insertedValue);
+            int i = statement.executeUpdate();
+            if (i >= 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
         }
     }
 }
